4d2b2121fb4cd68df48e27577efdece0bbce091c
Öktem Kalkan <oektem-kalkan@t-online.de>
2023-08-14 10:22:45 +0200

    add to shoppinCart

--- components/RapDetails.js
+++ components/RapDetails.js
@@ -20,6 +20,37 @@ export default function RapDetails() {
     return <h1>LOADING...</h1>;
   }
 
+  async function handleAddTicket(event) {
+    event.preventDefault();
+
+    const ticketData = {
+      image: data.image,
+      rating: data.rating,
+      name: data.name,
+      location: data.location,
+      date: data.date,
+      price: data.price,
+      currency: data.currency,
+    };
+
+    let cartTickets = JSON.parse(localStorage.getItem("cartTicket"));
+    if (!Array.isArray(cartTickets)) {
+      cartTickets = [];
+    }
+
+    const existingTicket = cartTickets.find(
+      (ticket) => ticket.name === ticketData.name
+    );
+    if (existingTicket) {
+      console.log(`A ticket from '${data.name}' is already in the cart.`);
+      return;
+    }
+
+    cartTickets.push(ticketData);
+
+    localStorage.setItem("cartTicket", JSON.stringify(cartTickets));
+  }
+
   return (
     <>
       <button onClick={() => router.push("/RapPage")}>back</button>
@@ -27,32 +58,23 @@ export default function RapDetails() {
         <Link href={"/shoppingCart"}>SHOPPING CART</Link>
       </Div>
       <article>
-        <Image src={data.image} width={"280"} height={"150"} alt="Artist" />
+        <Image
+          src={data.image}
+          priority="high"
+          alt="Rap Artist"
+          width={"280"}
+          height={"150"}
+        />
         <p>{data.rating}</p>
         <h4>{data.name}</h4>
+
         <div>
           <p>{data.location}</p>
           <p>{data.date}</p>
-          <p>
+
+          <button onClick={handleAddTicket}>
             {data.price} {data.currency}
-          </p>
-          <button>Add to Cart</button>
-        </div>
-        <div>
-          <p>{data.locationTwo}</p>
-          <p>{data.dateTwo}</p>
-          <p>
-            {data.price} {data.currency}
-          </p>
-          <button>Add to Cart</button>
-        </div>
-        <div>
-          <p>{data.locationThree}</p>
-          <p>{data.dateThree}</p>
-          <p>
-            {data.price} {data.currency}
-          </p>
-          <button>Add to Cart</button>
+          </button>
         </div>
       </article>
     </>
--- pages/shoppingCart.js
+++ pages/shoppingCart.js
@@ -1,16 +1,51 @@
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 import Image from "next/image";
 
-export default function ShoppingCart() {
+export default function ShoppingCart({ ticket }) {
   const router = useRouter();
+  const [cartTickets, setCartTickets] = useState([]);
 
   const goBack = () => {
     router.back();
   };
+
+  useEffect(() => {
+    const storedCartTickets = JSON.parse(localStorage.getItem("cartTicket"));
+    if (storedCartTickets && storedCartTickets.length > 0) {
+      setCartTickets(storedCartTickets);
+    }
+  }, []);
+
   return (
     <>
       <h2>Shopping Cart Page</h2>
       <button onClick={goBack}>back</button>
+
+      {cartTickets && cartTickets.length > 0 ? (
+        <ul>
+          {cartTickets.map((ticket, id) => (
+            <li key={id}>
+              <Image
+                src={ticket.image}
+                alt="Ticket"
+                priority="high"
+                width={"280"}
+                height={"150"}
+              />
+              <p>{ticket.rating}</p>
+              <p>{ticket.name}</p>
+              <p>{ticket.location}</p>
+              <p>{ticket.date}</p>
+              <p>
+                {ticket.price} {ticket.currency}
+              </p>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>Cart is empty</p>
+      )}
     </>
   );
 }
